401412a47c958c48f84fbef8f722ba9d7e27ffde
jaketrent <trent.jake@gmail.com>
2018-10-30 15:43:30 -0600

    test: update jsdom and shouldjs

--- test/utils/dom.js
+++ test/utils/dom.js
@@ -1,17 +1,6 @@
-import jsdom from 'jsdom'
+import { JSDOM } from 'jsdom'
 
-const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
-const win = doc.defaultView
-global.document = doc
-global.window = win
-propagateToGlobal(win)
-
-// from mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
-function propagateToGlobal (window) {
-  for (let key in window) {
-    if (!window.hasOwnProperty(key)) continue
-    if (key in global) continue
-
-    global[key] = window[key]
-  }
-}
+const dom = new JSDOM('<!doctype html><html><body></body></html>')
+global.window = dom.window
+global.document = dom.window.document
+global.navigator = global.window.navigator
